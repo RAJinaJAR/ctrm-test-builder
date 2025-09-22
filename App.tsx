6cc5e1f4c1be3e6845715f28a160c75a173be001
Raj Dhapse <rajdhapse@gmail.com>
2025-09-22 18:28:36 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -488,7 +488,7 @@ const App: React.FC = () => {
                 aria-label="Publish test package to Google Drive"
               >
                 {isPublishing ? <LoadingSpinnerIcon className="w-5 h-5 mr-2 text-white"/> : null}
-                {isPublishing ? 'Publishing...' : 'Publish to Google Drive'}
+                {isPublishing ? 'Publishing...' : 'Publish Test'}
               </button>
             </div>
           </aside>
