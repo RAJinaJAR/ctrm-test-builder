e4a918127d39ed321584e468f6976d1d22a25fbd
Raj Dhapse <rajdhapse@gmail.com>
2025-09-26 14:20:18 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,10 +1,8 @@
-
 import { BoxType } from './types';
 
 export const MAX_VIDEO_SIZE_MB = 200;
 export const MAX_VIDEO_SIZE_BYTES = MAX_VIDEO_SIZE_MB * 1024 * 1024;
-export const NUM_FRAMES_TO_EXTRACT = 10; // Number of frames to extract
-export const FRAME_EXTRACTION_TARGET_FPS = 1; // Try to extract 1 frame per N seconds of video duration, up to NUM_FRAMES_TO_EXTRACT
+export const FRAME_EXTRACTION_INTERVAL_SECONDS = 5; // Extract one frame every 5 seconds
 
 export const BOX_COLORS: Record<BoxType, { bg: string; border: string }> = {
   [BoxType.HOTSPOT]: { bg: 'bg-transparent', border: 'border-none' }, // Tailwind opacity format
@@ -15,5 +13,3 @@ export const SELECTED_BOX_BORDER_COLOR = 'border-yellow-400 ring-2 ring-yellow-4
 
 export const DEFAULT_BOX_SIZE_PERCENT = { w: 15, h: 5 }; // Default new box size as % of image
 export const DEFAULT_BOX_POSITION_PERCENT = { x: 42.5, y: 45 }; // Default new box position as % of image
-
-    
